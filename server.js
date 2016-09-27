00f7f8899eb5456d8ed63446cc21be4fc22b759a
baaraath <baraththebrave@gmail.com>
2016-09-27 12:55:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ app.get('/Article-one',function(req, res){
 });
 
 app.get('/Article-two',function(req, res){
-    res.send('Article two is requested and it will be served');
+    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 app.get('/Article-three',function(req, res){
-    res.send('Article three is requested and it will be served');
+     res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 
