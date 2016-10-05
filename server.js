ae253d1230321d1de4e09b9e820a05e676a61b43
baaraath <baraththebrave@gmail.com>
2016-10-05 19:36:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,13 +4,99 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+var content ={
+     title:'article-one|barath',
+     heading:'article one',
+     date:'3rd oct 2016',
+     content:`<html>
+ <head>
+    <title>
+         Article one |barath
+        </title>
+<meta name="viewport" content="width=device-width, initial-scale=1"/>
+<link href="/ui/style.css" rel="stylesheet" />
+</head>
+<body>
+    <div>
+        <a href="/">home</a>
+    </div>
+<hr/>
+<h3>
+    Article one
+</h3> 
+<div class="container">
+    
+
+<div>
+    3rd oct 2016
+</div>
+<div>
+    <p>
+     this is the content of the first article this is the content of the first article this is the content of the first articlethis is the content of the first article this is the content of the first article this is the content of the first article this is the content of the first article this is the content of the first article this is the content of the first article this is the content of the first articlethis is the content of the first article this is the content of the first article
+</p>
+ <p>
+     this is the content of the first article this is the content of the first article this is the content of the first articlethis is the content of the first article this is the content of the first article this is the content of the first article this is the content of the first article this is the content of the first article this is the content of the first article this is the content of the first articlethis is the content of the first article this is the content of the first article
+</p>
+ <p>
+     this is the content of the first article this is the content of the first article this is the content of the first articlethis is the content of the first article this is the content of the first article this is the content of the first article this is the content of the first article this is the content of the first article this is the content of the first article this is the content of the first articlethis is the content of the first article this is the content of the first article
+</p>
+</div>
+</div>
+</body>
+</html>
+`
+     
+};
+function createTemplate (data){
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+}
+
+var htmlTemplate= `
+    <html>
+ <head>
+    <title>
+         ${title}
+         </title>
+<meta name="viewport" content="width=device-width, initial-scale=1"/>
+<link href="/ui/style.css" rel="stylesheet" />
+</head>
+<body>
+    <div>
+        <a href="/">home</a>
+    </div>
+<hr/>
+<h3>
+    ${heading}
+</h3> 
+<div class="container">
+    
+
+<div>
+    
+    ${date}
+    </div>
+<div>
+    <p>
+  ${content}
+</p>
+</div>
+</div>
+</body>
+</html>
+`;
+return htmlTemplate;
+
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one',function (req,res){
-res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));   
+res.send(createTemplate(articleone));   
 });
 
 app.get('/article-two',function (req,res){
