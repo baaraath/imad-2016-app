a2115aa9d61b36d2c56f8e8be65ac1705c6c2af6
baaraath <baraththebrave@gmail.com>
2016-10-10 14:18:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,5 +3,12 @@ var element = document.getElementById('main-text');
 element.innerHTML= 'hello krishna';
 var img = document.getElementById('madi');
 img.onclick = function () {
-    img.style.marginLeft='100px';
+var marginLeft=0;
+ function moveRight()
+ {
+     marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+'px';
+}
+var interval=setInterval(moveRight, 100);
+    
 };
