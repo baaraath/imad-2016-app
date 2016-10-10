dc990e4bd81983cf692c73abf108796c16e9110c
baaraath <baraththebrave@gmail.com>
2016-10-10 13:58:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,3 @@
 console.log('Loaded!');
-var element=document.getElementById('main-text');
-element.innerHtml= "hello krishna";
+var element = document.getElementById('main-text');
+element.innerHtml= 'hello krishna';
