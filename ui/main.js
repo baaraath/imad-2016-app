628139675066740411790611d861d674f61da6a4
baaraath <baraththebrave@gmail.com>
2016-10-11 13:32:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,13 +2,13 @@ var button=document.getElementById(`counter`);
 button.onclick = function(){
     var request = new XMLHttpRequest();
     request.onreadystatechange=function(){
-        if(request.readyState===XMlHttpRequest.DONE)
+        if(request.readyState===XMlHttpRequest.DONE){
    if(request.status===200)
    {
        var counter=request.responseText;
     var span=document.getElementById(`count`);
     span.innerHTML=counter.toString();
-    }
+    }}
 }; 
 request.open('GET',`http://baaraath.imad.hasura-app.io/`,true);
 request.send(null);
