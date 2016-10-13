a69747ccf0f5b2ab8205b003dc0015052d3f26df
baaraath <baraththebrave@gmail.com>
2016-10-13 18:20:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,15 +17,18 @@ request.send(null);
   
   };
   var nameInput=document.getElementById(`name`);
-  var name=nameInput.value;
+  var name = nameInput.value;
   var submit=document.getElementById(`submit_btn`);
+   var ul=document.getElementById(`namelist`);
   submit.onclick=function() {
       var names=[`name1`,`name2`,`name3`,`name4`];
       var list=``;
       for(var i=0;i<names.length;i++)
-      list += `<li>`+names[i]+`</li>` ;
+      list = `<li>`+names[i]+`</li>`;
+      
   }
-  var ul=document.getElementById(`namelist`);
   ul.innerHTML=list;
+  
+  
    };
   
